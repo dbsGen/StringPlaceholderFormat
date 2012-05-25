bf04084c977e9c2d46d91f49f69d6c2a08b5751b
dbsGen <z.rz2323721@gmail.com>
2012-05-25 16:05:54 +0800

    update

--- NSString+MTExtend.m
+++ NSString+MTExtend.m
@@ -16,13 +16,14 @@
     const char *ch = st;
     if (strlen(ch) > 2) {
         ch += 2;
-        int index[10], count = 0;
-        for (int i = 0 ; i < 10; i ++) {
-            index[i] = -1;
-        }
+        int count = 0;
+        char index[10][4];
+        memset(index, 0, 30*sizeof(char));
         while (*ch) {
             if (*(ch - 2) == '{' && *ch == '}' && *(ch - 1) >= '0' && *(ch - 1) <= '9') {
-                index[count] = (ch - 1) - st;
+                index[count][0] = '{';
+                index[count][1] = *(ch - 1);
+                index[count][2] = '}';
                 count++;
             }
             ch ++;
@@ -36,15 +37,7 @@
             va_start(arglist, format);
             for (int n = 0; n < count; n++) {
                 NSString *string = va_arg(arglist, id);
-                int i = index[n];
-                int value = st[i] - '0';
-                if (i < 0) {
-                    free(mem);
-                    @throw [NSException exceptionWithName:@"String format error"
-                                                   reason:@"wrong format"
-                                                 userInfo:nil];
-                    return nil;
-                }
+                int value = index[n][1] - '0';
                 mem[value] = string;
             }
             va_end(arglist);
@@ -62,9 +55,10 @@
             }
             
             for (int n = count - 1; n >= 0; n--) {
-                int i = index[n];
-                [nFormat replaceCharactersInRange:(NSRange){i - 1, 3}
-                                       withString:mem[n]];
+                [nFormat replaceOccurrencesOfString:[NSString stringWithUTF8String:index[n]] 
+                                         withString:mem[n] 
+                                            options:NSLiteralSearch 
+                                              range:(NSRange){0, [nFormat length]}];
             }
             free(mem);
             NSString *output = [NSString stringWithString:nFormat];
@@ -75,7 +69,7 @@
     va_list arglist;
     va_start(arglist, format);
     NSString *output = [[[NSString alloc] initWithFormat:format
-                                              arguments:arglist] autorelease];
+                                               arguments:arglist] autorelease];
     va_end(arglist);
     return output;
 }
